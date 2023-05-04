2c8784281b176f66ee1461a7c4687879994c64dd
Williams Espinoza <espinoza6006@gmail.com>
2023-05-04 14:42:50 +0200

    Python Pildoras Informaticas. Video 5. Sintaxis basica III. Funciones I

---
+++ Video5. Sintaxis Básica III. Funciones I.py
@@ -0,0 +1,13 @@
+#Creación de funciones: 
+
+def mensaje_Usuario(): # Declaración de la función
+    print("Williams Estas Aprendiendo Python") #Lo que esta dentro de la función se llama Cuerpo de la función. 
+    print()                                    # Cuerpo de la función. 
+    print("Capitulo de Funciones en Python")   # Cuerpo de la función.
+    print()                                    # Cuerpo de la función.
+    print("Paso a paso será un excelente programdor Python y Devops") # Cuerpo de la función.
+
+# Para llamar a la función es necesario escribir el nombre de la función tal como se muestra abajo. 
+mensaje_Usuario() # Llamada de la función.
+print("Ejecutando codigo fuera de la función.") # Podemos colocar bloques de cogido que no tienen nada que ver con la función. 
+mensaje_Usuario()
