8f3800a77c8d9d48c591bb5e5f55c7e69ef97fce
Williams Espinoza <espinoza6006@gmail.com>
2023-05-04 17:54:53 +0200

    Python Pildoras Informaticas. Video 6. Sintaxis basica IV. Funciones II. comentarios

--- Video6. Sintaxis Basica IV Funciones II.py
+++ Video6. Sintaxis Basica IV Funciones II.py
@@ -1,4 +1,5 @@
 # Continuamos con las funciones en este capitulo veremos mas en profundidad el tema de funciones con y sin parámetros. 
+# En Python se pasan los valores por referencia en las funciones. 
 
 def sumar():
     numero1=2.5
@@ -16,12 +17,13 @@ sumarParametros(1,5)
 sumarParametros(100,200)
 sumarParametros(5.5,2)
 
-#Declaración de funciones que devulven un valor return
+#Declaración de funciones que devulven un valor return --> Nomalmente se utiliza para almecenar el valor de la función en una variable
 print()
 print("este el valor de la suma de dos valores utilizando funciones con parametros y que retorne un valor")
 def sumarParametrosReturn(numero1,numero2):
-    resultado = numero1+numero2
+    resultado = numero1+numero2  #Declaro una variable dentro de la función que tendra como misión almacenar el valor de la suma de los dos numeros. 
     return resultado
 
-resultado= sumarParametrosReturn(1,5)
-print(resultado)
+almacenaResultado= sumarParametrosReturn(1,5) # Creo una variable nueva y alamceno el valor de 
+print(almacenaResultado)
+
