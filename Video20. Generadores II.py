b879158966ffd02c4c33bb68d72b784606eba939
Williams Espinoza <espinoza6006@gmail.com>
2023-05-18 20:14:10 +0200

    Video20. Generadores II. yield from

---
+++ Video20. Generadores II.py
@@ -0,0 +1,17 @@
+# Crear una función generadora que nos devuelva uuna serie de ciudades 
+
+def devulveciudades(*ciudades): # En python cuando utilizamos * le estamos indicando que va recibir un numiro interminado de elementos. y ademas esos elementos los va a recibir en forma de tupla por defecto. 
+    for elemento in ciudades:    # Primer bucle for para el primer elemento que se almacena en el objeto generador
+        #for subelemento in elemento: # segundo bucle for para conocer los subelementos de ese primer elemento (que este caso son caracteres o letras)
+         #   yield subelemento
+        yield from elemento
+
+
+ciudades_devueltas = devulveciudades("Madrid", "Barcelona", "Caracas", "Paris")
+
+print(next(ciudades_devueltas))
+print(next(ciudades_devueltas))
+
+
+
+
